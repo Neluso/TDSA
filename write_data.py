d9795d46a49e86d902bd32a24d8c7a2663fa1cfa
Neluso <neluso.mu@gmail.com>
2019-11-14 11:13:01 +0100

    Adjusted write_data.py

--- write_data.py
+++ write_data.py
@@ -11,8 +11,8 @@ def write_data(freq, E_ref_w, E_sam_w):
     wh_ref = open(dir_path + '/ref_spectrum.txt', 'w')
     wh_sam = open(dir_path + '/sam_spectrum.txt', 'w')
     for i in range(freq.size):
-        wh_ref.write(str(freq[i]) + ',' + str(E_ref_w[i]))
-        wh_sam.write(str(freq[i]) + ',' + str(E_sam_w[i]))
+        wh_ref.write(str(freq[i]) + ',' + str(E_ref_w[i]) + '\n')
+        wh_sam.write(str(freq[i]) + ',' + str(E_sam_w[i]) + '\n')
     wh_ref.close()
     wh_sam.close()
     return 0
