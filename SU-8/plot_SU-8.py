60fdce6ced83106dd4c9d6b12c8bf43a462c17dc
Neluso <neluso.mu@gmail.com>
2021-01-25 15:50:49 +0100

    plot_SU-8

--- SU-8/plot_SU-8.py
+++ SU-8/plot_SU-8.py
@@ -1,5 +1,5 @@
 from TDSA import *
-from scipy.interpolate import Akima1DInterpolator
+from scipy.interpolate import Akima1DInterpolator, interp1d
 
 
 def nk_from_eps(e_w):
@@ -10,12 +10,17 @@ def nk_from_eps(e_w):
 
 f_real, eps_real = read_1file('./real_fully_cross.csv')
 f_imag, eps_imag = read_1file('./imag_fully_cross.csv')
+# plot(f_imag, eps_imag, '-')
+# show()
+# quit()
 
-e_r = Akima1DInterpolator(f_real, eps_real)
-e_i = Akima1DInterpolator(f_imag, eps_imag)
-
+e_r = interp1d(f_real, eps_real, fill_value='extrapolate')
+e_i = interp1d(f_imag, eps_imag, fill_value='extrapolate')
 
 n, k = nk_from_eps(e_r(f_real) + 1j * e_i(f_real))
+nh = open('./n_SU8.txt', 'w')
+kh = open('./k_SU8.txt', 'w')
 
-plot(f_real, n)
-show()
+for i in range(f_real.size):
+    nh.write(str(f_real[i]) + ',' + str(n[i]) + '\n')
+    kh.write(str(f_real[i]) + ',' + str(k[i]) + '\n')
