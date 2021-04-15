9df53e3f49a5fa8145f990e766f4f7ba7f49f90d
Neluso <neluso.mu@gmail.com>
2021-04-15 08:38:18 +0200

    adiabatic 3

--- adiabatic_index.py
+++ adiabatic_index.py
@@ -36,45 +36,85 @@ def f_n_1(n_1, n_2, D_adiab, d):
     return m * d + b
 
 
-D_adiab = 1e-5  # 100 um
-n_1 = 1.4 + 1j * 0.01
-thick_1 = 1e-4  # 1 mm
-n_2 = 1.6 + 1j * 0.01
-thick_2 = 1e-4  # 1 mm
-m = (n_2 - n_1) / D_adiab
-b = n_1
-N_grid = 2
-d = D_adiab / N_grid
 t_ref, E_ref = read_1file('./data/demo_data/ref.txt')
+delta_t_ref = mean(diff(t_ref))
+enlargement = 0 * E_ref.size
+ref_pulse_idx = centre_loc(E_ref)
+E_ref = zero_padding(E_ref, 0, enlargement)
+t_ref = concatenate((t_ref, t_ref[-1] * ones(enlargement) + delta_t_ref * arange(1, enlargement + 1)))
 t_ref *= 1e-12
 f_ref, E_ref_w = fourier_analysis(t_ref, E_ref)
-M_i = list()
+# [1, 2, 10, 50, 100, 1000]:
+for N_grid in [1000, 10000, 100000]:
+    D_adiab = 1e-5  # 10 um
+    n_1 = 1.4 + 1j * 0.03
+    thick_1 = 1e-4 - D_adiab/2  # 100 um
+    n_2 = 1.6 + 1j * 0.03
+    thick_2 = 1e-4 - D_adiab/2  # 100 um
+    m = (n_2 - n_1) / D_adiab
+    b = n_1
+    # N_grid = 10
+    d = D_adiab / N_grid
 
-for freq in f_ref:
-    m_in = dot(P_i(n_1, thick_1, freq), D_i_j(n_air, n_1))
-    m_i = m_in
-    for i in range(N_grid - 1):
-        n_i = f_n_1(n_1, n_2, D_adiab, d * (i + 1))  # n_i
-        n_i_1 = f_n_1(n_1, n_2, D_adiab, d * (i + 2))  # n_i+1
-        phase_mat_i = P_i(n_i, d, freq)
+    M_i = list()
+
+    for freq in f_ref:
+        m_in = dot(P_i(n_1, thick_1, freq), D_i_j(n_air, n_1))
+        m_i = m_in
+        for i in range(N_grid):
+            n_i = f_n_1(n_1, n_2, D_adiab, d * (i + 1))  # n_i
+            n_i_1 = f_n_1(n_1, n_2, D_adiab, d * (i + 2))  # n_i+1
+            phase_mat_i = P_i(n_i, d, freq)
+            fres_mat_i = D_i_j(n_i, n_i_1)
+            m_j = dot(phase_mat_i, fres_mat_i)
+            m_i = dot(m_i, m_j)
+        n_i = n_2  # f_n_1(n_1, n_2, D_adiab, D_adiab - d)  # n_i
+        n_i_1 = n_air  # n_i+1
+        phase_mat_i = P_i(n_i, thick_2, freq)
         fres_mat_i = D_i_j(n_i, n_i_1)
-        m_j = dot(phase_mat_i, fres_mat_i)
-        m_i = dot(m_i, m_j)
-    n_i = f_n_1(n_1, n_2, D_adiab, D_adiab - 1)  # n_i
-    n_i_1 = n_2  # n_i+1
-    phase_mat_i = P_i(n_i, thick_2, freq)
-    fres_mat_i = D_i_j(n_i, n_i_1)
-    m_out = dot(phase_mat_i, fres_mat_i)
-    m_i = dot(m_i, m_out)
-    M_i.append(m_i)
-M_i = array(M_i)
-M_11 = M_i[:, 0, 0]
-M_21 = M_i[:, 1, 0]
+        m_out = dot(phase_mat_i, fres_mat_i)
+        m_i = dot(m_i, m_out)
+        M_i.append(m_i)
 
-phi_air = 1  # phase_factor(n_air, -(thick_1 + thick_2 + D_adiab), f_ref)
-H_r = M_21 / M_11 * phi_air
-H_t = 1 / M_11 * phi_air
-E_sim = irfft(H_t * E_ref_w)
-plot(t_ref, E_ref)
-plot(t_ref, E_sim)
+    M_i = array(M_i)
+    M_11 = M_i[:, 0, 0]
+    M_21 = M_i[:, 1, 0]
+    M_12 = M_i[:, 0, 1]
+    M_22 = M_i[:, 1, 1]
+
+    phi_air = phase_factor(n_air, - (thick_1 + thick_2 + D_adiab), f_ref)
+    H_r = - M_21 / M_22 * phi_air
+    H_t = (M_11 - M_12*M_21 / M_22) * phi_air
+
+    E_sim_r = irfft(H_r * E_ref_w)
+    E_sim_t = irfft(H_t * E_ref_w)
+    E_sim_r_w = H_r * E_ref_w
+    E_sim_t_w = H_t * E_ref_w
+    angle_r = jepsen_unwrap(t_ref, E_ref, t_ref, E_sim_r)
+    angle_t = jepsen_unwrap(t_ref, E_ref, t_ref, E_sim_t)
+    figure(1)
+    # plot(t_ref * 1e12, E_ref, '--', label='ref', lw=1)
+    title('R wave')
+    plot(t_ref * 1e12, E_sim_r, label=str(N_grid), lw=1)
+    legend()
+    figure(2)
+    title('T wave')
+    plot(t_ref * 1e12, E_sim_t, label=str(N_grid), lw=1)
+    legend()
+    figure(3)
+    # plot(t_ref * 1e12, E_ref, '--', label='ref', lw=1)
+    title('R wave')
+    plot(f_ref * 1e-12, angle_r, label=str(N_grid), lw=1)
+    legend()
+    figure(4)
+    title('T wave')
+    plot(f_ref * 1e-12, angle_t, label=str(N_grid), lw=1)
+    legend()
+    # plot(t_ref * 1e12, E_ref / amax(abs(E_ref)))
+    # plot(t_ref * 1e12, E_sim / amax(abs(E_sim)))
+
+    # figure(2)
+    # plot(f_ref * 1e-12, abs(H_r))
+    # plot(f_ref * 1e-12, abs(H_t))
+
 show()
