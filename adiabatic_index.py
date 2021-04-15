3c7d435a98b78de586c50853e5a61773c4605aee
Neluso <neluso.mu@gmail.com>
2021-04-15 10:30:29 +0200

    adiabatic 3

--- adiabatic_index.py
+++ adiabatic_index.py
@@ -45,7 +45,7 @@ t_ref = concatenate((t_ref, t_ref[-1] * ones(enlargement) + delta_t_ref * arange
 t_ref *= 1e-12
 f_ref, E_ref_w = fourier_analysis(t_ref, E_ref)
 # [1, 2, 10, 50, 100, 1000]:
-for N_grid in [1000, 10000, 100000]:
+for N_grid in [10, 50]:
     D_adiab = 1e-5  # 10 um
     n_1 = 1.4 + 1j * 0.03
     thick_1 = 1e-4 - D_adiab/2  # 100 um
@@ -57,6 +57,7 @@ for N_grid in [1000, 10000, 100000]:
     d = D_adiab / N_grid
 
     M_i = list()
+    N_i = list()
 
     for freq in f_ref:
         m_in = dot(P_i(n_1, thick_1, freq), D_i_j(n_air, n_1))
@@ -74,7 +75,9 @@ for N_grid in [1000, 10000, 100000]:
         fres_mat_i = D_i_j(n_i, n_i_1)
         m_out = dot(phase_mat_i, fres_mat_i)
         m_i = dot(m_i, m_out)
+        n_i = dot(m_out, m_in)
         M_i.append(m_i)
+        N_i.append(n_i)
 
     M_i = array(M_i)
     M_11 = M_i[:, 0, 0]
@@ -82,9 +85,17 @@ for N_grid in [1000, 10000, 100000]:
     M_12 = M_i[:, 0, 1]
     M_22 = M_i[:, 1, 1]
 
+    N_i = array(N_i)
+    N_11 = N_i[:, 0, 0]
+    N_21 = N_i[:, 1, 0]
+    N_12 = N_i[:, 0, 1]
+    N_22 = N_i[:, 1, 1]
+
     phi_air = phase_factor(n_air, - (thick_1 + thick_2 + D_adiab), f_ref)
     H_r = - M_21 / M_22 * phi_air
     H_t = (M_11 - M_12*M_21 / M_22) * phi_air
+    # H_r_2lay = - M_21 / M_22 * phi_air
+    # H_t_2lay = (M_11 - M_12 * M_21 / M_22) * phi_air
 
     E_sim_r = irfft(H_r * E_ref_w)
     E_sim_t = irfft(H_t * E_ref_w)
@@ -92,6 +103,7 @@ for N_grid in [1000, 10000, 100000]:
     E_sim_t_w = H_t * E_ref_w
     angle_r = jepsen_unwrap(t_ref, E_ref, t_ref, E_sim_r)
     angle_t = jepsen_unwrap(t_ref, E_ref, t_ref, E_sim_t)
+
     figure(1)
     # plot(t_ref * 1e12, E_ref, '--', label='ref', lw=1)
     title('R wave')
@@ -116,5 +128,34 @@ for N_grid in [1000, 10000, 100000]:
     # figure(2)
     # plot(f_ref * 1e-12, abs(H_r))
     # plot(f_ref * 1e-12, abs(H_t))
+
+H_r_2lay = - M_21 / M_22 * phi_air
+H_t_2lay = (M_11 - M_12 * M_21 / M_22) * phi_air
+
+E_sim_r_2lay = irfft(H_r * E_ref_w)
+E_sim_t_2lay = irfft(H_t * E_ref_w)
+E_sim_r_w_2lay = H_r * E_ref_w
+E_sim_t_w_2lay = H_t * E_ref_w
+angle_r_2lay = jepsen_unwrap(t_ref, E_ref, t_ref, E_sim_r_2lay)
+angle_t_2lay = jepsen_unwrap(t_ref, E_ref, t_ref, E_sim_t_2lay)
+
+figure(1)
+# plot(t_ref * 1e12, E_ref, '--', label='ref', lw=1)
+title('R wave')
+plot(t_ref * 1e12, E_sim_r_2lay, label='2_lay', lw=1)
+legend()
+figure(2)
+title('T wave')
+plot(t_ref * 1e12, E_sim_t_2lay, label='2_lay', lw=1)
+legend()
+figure(3)
+# plot(t_ref * 1e12, E_ref, '--', label='ref', lw=1)
+title('R wave')
+plot(f_ref * 1e-12, angle_r_2lay, label='2_lay', lw=1)
+legend()
+figure(4)
+title('T wave')
+plot(f_ref * 1e-12, angle_t_2lay, label='2_lay', lw=1)
+legend()
 
 show()
