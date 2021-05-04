d2b7ffae0a0975c7a53e86a0ec8d1d2ea7ec2d95
Neluso <neluso.mu@gmail.com>
2021-05-04 18:23:23 +0200

    adiabatic 11

--- adiabatic/adiabatic_index_rouard.py
+++ adiabatic/adiabatic_index_rouard.py
@@ -44,10 +44,12 @@ def H_sim(freq, n_l, n_l_1, thick, H_subs):
 def f_n_1(n_1, n_2, D_adiab, d):
     m = (n_2 - n_1) / D_adiab
     b = n_1
-    return m * d + b
+    return m * d + b  # linar transition
+    # return m * d**2 / D_adiab + b  # quadratic transition
+    # return m * d**3 / D_adiab**2 + b  # cubic transition
 
 
-t_ref, E_ref = read_1file('./data/demo_data/ref.txt')
+t_ref, E_ref = read_1file('../data/demo_data/ref.txt')
 # t_ref = arange(1, 5001) * 0.05e-12
 # f_carrier = 0.3e12  # 300 Ghz
 # E_ref = sin(2 * pi * t_ref * f_carrier)
@@ -56,7 +58,7 @@ t_ref, E_ref = read_1file('./data/demo_data/ref.txt')
 
 
 delta_t_ref = mean(diff(t_ref))
-enlargement = 0 * E_ref.size
+enlargement = 2 * E_ref.size
 ref_pulse_idx = centre_loc(E_ref)
 E_ref = zero_padding(E_ref, 0, enlargement)
 t_ref = concatenate((t_ref, t_ref[-1] * ones(enlargement) + delta_t_ref * arange(1, enlargement + 1)))
@@ -64,9 +66,9 @@ t_ref *= 1e-12
 f_ref, E_ref_w = fourier_analysis(t_ref, E_ref)
 D_adiab = 5e-3  # 100 um
 n_1 = 1.4 - 1j * 0.03
-thick_1 = 1e-4  # - D_adiab/2  # 1000 um
+thick_1 = 1e-3  # - D_adiab/2  # 1000 um
 n_2 = 2.6 - 1j * 0.03
-thick_2 = 1e-4  # - D_adiab/2  # 1000 um
+thick_2 = 1e-3  # - D_adiab/2  # 1000 um
 # phi_air = phase_factor(n_air, - thick_1 - thick_2, f_ref)
 phi_air = 1
 m = (n_2 - n_1) / D_adiab
@@ -84,12 +86,13 @@ E_sim_nad = irfft(H_teo_no_adiab * E_ref_w)
 N_grid = 10000
 # for D_adiab in [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]:  # 1000 és suficient per a simular adiabàtic
 # for D_adiab in [1e-6, 10**-5.5, 1e-5, 10**-4.5, 1e-4, 10**-3.5, 1e-3]:
-for D_adiab in [1e-6, 1e-5, 1e-4]:  # , 1e-4, 1.5e-4]:  # , 1e-3]:  # 1000 és suficient per a simular adiabàtic
+for D_adiab in [1e-5, 10**-4.9, 10**-4.8, 10**-4.7, 10**-4.6, 10**-4.5, 10**-4.4, 10**-4.3, 10**-4.2, 10**-4.1, 1e-4, 10**-3.9, 10**-3.8, 10**-3.7, 10**-3.6, 10**-3.5, 10**-3.4, 10**-3.3, 10**-3.2, 10**-3.1, 1e-3]:
+# for D_adiab in [1e-6, 1e-5, 1e-4, 1e-3]:  # , 1e-4, 1.5e-4]:  # , 1e-3]:  # 1000 és suficient per a simular adiabàtic
     # # "d" adaptable, N_grid fixe
-    # d = D_adiab / N_grid
+    d = D_adiab / N_grid
     # # "d" fixe, N_grid adaptable
-    d = 1e-10
-    N_grid = int(D_adiab / d)
+    # d = 1e-10
+    # N_grid = int(D_adiab / d)
     n_adiab = (arange(N_grid) + 0.5) * d
     n_adiab = f_n_1(n_1, n_2, D_adiab, n_adiab)
     n_adiab = append(n_1, n_adiab)
@@ -114,15 +117,15 @@ for D_adiab in [1e-6, 1e-5, 1e-4]:  # , 1e-4, 1.5e-4]:  # , 1e-3]:  # 1000 és s
     legend_Text = str(D_adiab * 1e6) + ' ' + 'um'
     figure(1)
     plot(t_ref * 1e12, E_sim_ad, label=legend_Text, lw=1)
-    figure(2)
-    plot(f_ref * 1e-12, abs(H_teo_adiab), label=legend_Text, lw=1)
+    # figure(2)
+    # plot(f_ref * 1e-12, abs(H_teo_adiab), label=legend_Text, lw=1)
     figure(3)
     plot(t_ref * 1e12, irfft(H_teo_adiab), label=legend_Text, lw=1)
     figure(4)
     plot(t_ref * 1e12, real(irfft(H_teo_adiab - H_teo_no_adiab)), label=legend_Text, lw=1)
     print(D_adiab * 1e6, 'um ->', sum(abs(irfft(H_teo_adiab - H_teo_no_adiab))) * 1e3)
-    figure(5)
-    plot(D_adiab * 1e6, sum(abs(irfft(H_teo_adiab - H_teo_no_adiab))), 'b.')
+    # figure(5)
+    # plot(D_adiab * 1e6, sum(abs(irfft(H_teo_adiab - H_teo_no_adiab))), 'b.')
 
 
 n_eff = (n_1 + n_2) / 2
@@ -137,18 +140,18 @@ figure(1)
 title('R wave')
 plot(t_ref * 1e12, E_sim_nad, '--', label='2_lay', lw=1)
 # plot(t_ref * 1e12, E_sim_effad, '-.', label='2_lay_eff', lw=1)
-legend()
-figure(2)
-plot(f_ref * 1e-12, abs(H_teo_no_adiab), '--', label='2_lay', lw=1)
+# legend()
+# figure(2)
+# plot(f_ref * 1e-12, abs(H_teo_no_adiab), '--', label='2_lay', lw=1)
 # plot(f_ref * 1e-12, abs(H_teo_eff_adiab), '-.', label='2_lay_eff', lw=1)
-legend()
+# legend()
 figure(3)
 plot(t_ref * 1e12, irfft(H_teo_no_adiab), '--', label='2_lay', lw=1)
 # plot(t_ref * 1e12, abs(irfft(H_teo_eff_adiab)), '-.', label='2_lay_eff', lw=1)
-legend()
+# legend()
 figure(4)
-legend()
-figure(5)
-xlabel('D_adiab')
-ylabel('sum diff H')
+# legend()
+# figure(5)
+# xlabel('D_adiab')
+# ylabel('sum diff H')
 show()
