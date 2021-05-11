cb1e66906ecc60630b09212e93757a8a7d726a36
Neluso <neluso.mu@gmail.com>
2021-05-11 12:53:22 +0200

    adiabatic 15

--- adiabatic/adiabatic_index_rouard.py
+++ adiabatic/adiabatic_index_rouard.py
@@ -58,17 +58,17 @@ t_ref, E_ref = read_1file('../data/demo_data/ref.txt')
 
 
 delta_t_ref = mean(diff(t_ref))
-enlargement = 1 * E_ref.size
+enlargement = 0 * E_ref.size
 ref_pulse_idx = centre_loc(E_ref)
 E_ref = zero_padding(E_ref, 0, enlargement)
 t_ref = concatenate((t_ref, t_ref[-1] * ones(enlargement) + delta_t_ref * arange(1, enlargement + 1)))
 t_ref *= 1e-12
 f_ref, E_ref_w = fourier_analysis(t_ref, E_ref)
-D_adiab = 5e-3  # 100 um
-n_1 = 1.4 - 1j * 0.03
-thick_1 = 1e-3  # - D_adiab/2  # 1000 um
-n_2 = 1.8 - 1j * 0.03
-thick_2 = 1e-3  # - D_adiab/2  # 1000 um
+D_adiab = 2e-3  # 100 um
+n_1 = 1.45 - 1j * 0.03
+thick_1 = 2.5e-4  # - D_adiab/2  # 1000 um
+n_2 = 1.65 - 1j * 0.03
+thick_2 = 2.5e-4  # - D_adiab/2  # 1000 um
 # phi_air = phase_factor(n_air, - thick_1 - thick_2, f_ref)
 phi_air = 1
 m = (n_2 - n_1) / D_adiab
@@ -83,23 +83,28 @@ E_sim_nad = irfft(H_teo_no_adiab * E_ref_w)
 
 # [1, 2, 10, 50, 100, 1000]:
 # Building adiabatic samples
-N_grid = 10000
+N_grid = 10
 # for D_adiab in [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]:  # 1000 és suficient per a simular adiabàtic
 # for D_adiab in [1e-6, 10**-5.5, 1e-5, 10**-4.5, 1e-4, 10**-3.5, 1e-3]:
 # for D_adiab in [1e-5, 10**-4.9, 10**-4.8, 10**-4.7, 10**-4.6, 10**-4.5, 10**-4.4, 10**-4.3, 10**-4.2, 10**-4.1, 1e-4, 10**-3.9, 10**-3.8, 10**-3.7, 10**-3.6, 10**-3.5, 10**-3.4, 10**-3.3, 10**-3.2, 10**-3.1, 1e-3]:
-for D_adiab in [1e-5, 1e-4, 1e-3]:  # , 1e-4, 1.5e-4]:  # , 1e-3]:  # 1000 és suficient per a simular adiabàtic
+for D_adiab in [1e-5, 1e-4, 2e-4]:  # , 1e-4, 1.5e-4]:  # , 1e-3]:  # 1000 és suficient per a simular adiabàtic
     # # "d" adaptable, N_grid fixe
-    d = D_adiab / N_grid
+    # d = D_adiab / N_grid
     # # "d" fixe, N_grid adaptable
-    # d = 1e-8  # amstrong o nm? --> 10 nm
-    # N_grid = int(D_adiab / d)
+    d = 1e-6  # amstrong o nm? --> 10 nm
+    N_grid = int(D_adiab / d)
+    print(d * 1e6, 'um')
+    print('Grid:', N_grid)
     n_adiab = (arange(N_grid) + 0.5) * d
     n_adiab = f_n_1(n_1, n_2, D_adiab, n_adiab)
     n_adiab = append(n_1, n_adiab)
     n_adiab = append(n_adiab, n_2)
+    # n_adiab *= 0.01 * (2 * random.rand(N_grid + 2) - 1)
     d_adiab = d * ones(N_grid)
     d_adiab = append(thick_1 - D_adiab / 2, d_adiab)
     d_adiab = append(d_adiab, thick_2 - D_adiab / 2)
+    # d_adiab = append(thick_1, d_adiab)  # - D_adiab / 2, d_adiab)
+    # d_adiab = append(d_adiab, thick_2)  # - D_adiab / 2)
 
     n_adiab = append(n_air_cplx, n_adiab)
     d_adiab = append(0, d_adiab)
@@ -118,9 +123,9 @@ for D_adiab in [1e-5, 1e-4, 1e-3]:  # , 1e-4, 1.5e-4]:  # , 1e-3]:  # 1000 és s
     figure(1)
     plot(t_ref * 1e12, E_sim_ad, label=legend_Text, lw=1)
     # figure(2)
-    # plot(f_ref * 1e-12, abs(H_teo_adiab), label=legend_Text, lw=1)
-    # figure(3)
-    # plot(t_ref * 1e12, irfft(H_teo_adiab), label=legend_Text, lw=1)
+    # plot(f_ref * 1e-12, toDb(H_teo_adiab * E_ref_w), label=legend_Text, lw=1)
+    figure(3)
+    plot(t_ref * 1e12, irfft(H_teo_adiab), label=legend_Text, lw=1)
     # figure(4)
     # plot(t_ref * 1e12, real(irfft(H_teo_adiab - H_teo_no_adiab)), label=legend_Text, lw=1)
     # print(D_adiab * 1e6, 'um ->', sum(abs(irfft(H_teo_adiab - H_teo_no_adiab))) * 1e3)
@@ -147,13 +152,15 @@ ylabel('Amplitud (u.a.)')
 xlim([5, 45])
 legend()
 # figure(2)
-# plot(f_ref * 1e-12, abs(H_teo_no_adiab), '--', label='2_lay', lw=1)
+# plot(f_ref * 1e-12, toDb(H_teo_eff_adiab * E_ref_w), '--', label='2_lay', lw=1)
 # plot(f_ref * 1e-12, abs(H_teo_eff_adiab), '-.', label='2_lay_eff', lw=1)
-# legend()
-# figure(3)
-# plot(t_ref * 1e12, irfft(H_teo_no_adiab), '--', label='2_lay', lw=1)
+legend()
+figure(3)
+plot(t_ref, irfft(H_teo_no_adiab), '--', label='2_lay', lw=1)
+xlim([1, 4])
+ylim([-0.005, 0.001])
 # # plot(t_ref * 1e12, abs(irfft(H_teo_eff_adiab)), '-.', label='2_lay_eff', lw=1)
-# # legend()
+legend()
 # figure(4)
 # # legend()
 # # figure(5)
